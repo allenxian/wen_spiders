633f583f06a499d6cfea0bf8db18ed5ee5fddf0a
ruoningwen <ruoningwen@gmail.com>
2017-07-05 16:58:55 +0800

    update the spider (it can work now)

---
+++ kr/FlashesSpider.py
@@ -0,0 +1,58 @@
+from kr.config import REDIS_CLIENT
+from utils.base import BaseSpider
+import json
+
+
+class FlashesSpider(BaseSpider):
+    website = 'kr'
+    key_name = 'flashes'
+    # test url
+    # news flashes template
+    news_flashes_tpl = 'http://36kr.com/api/info-flow/newsflash_columns/newsflashes?b_id=%s&per_page=%s'
+
+    def __init__(self):
+        super(FlashesSpider, self).__init__()
+        self.limit = 15000
+
+    def parse_news_flashes(self, cur_id):
+        per_page = 20
+        cur_url = self.news_flashes_tpl % (cur_id.decode("utf-8"), per_page)
+        print(cur_url)
+        resp = self.p_get(cur_url).text
+        resp_json = json.loads(resp)
+        news_list = resp_json['data']['items']
+        for news in news_list:
+            news.update({'_id': news['id']})
+            try:
+                self.save_doc(self.coll, news)
+            except Exception as e:
+                print(e)
+
+
+    def make_id_set(self, begin_id):
+        good_id = begin_id
+        while good_id > self.limit:
+            good_id -= 20
+            REDIS_CLIENT.sadd(self.key,
+                              good_id)
+        print('--------> finish making id set <--------')
+        print('we have %s id now' % REDIS_CLIENT.scard(self.key))
+
+    def multi_thread(self):
+        pass
+
+    def process(self, begin_id):
+        # self.make_id_set(begin_id)
+        while REDIS_CLIENT.scard(self.key) > 0:
+            the_id = REDIS_CLIENT.spop(self.generate_key())
+            try:
+                self.parse_news_flashes(the_id)
+            except Exception as e:
+                print(e)
+                print('gg')
+                # REDIS_CLIENT.sadd(self.key, the_id)
+
+
+if __name__ == '__main__':
+    flash_spider = FlashesSpider()
+    flash_spider.process(67672)
--- utils/base.py
+++ utils/base.py
@@ -1,10 +1,16 @@
 import requests
 from utils.user_agents import random_ua
+from kr.config import MONGO_CLIENT, DB_NAME
 
 
-class base_spider(object):
+class BaseSpider(object):
+    website = 'base'
+    key_name = 'base'
+
     def __init__(self):
-        self.xsrf = ''
+        self.key = self.generate_key()
+        self.coll = self.db['%s_%s' % (self.website, self.key_name)]
+        self.HEADERS = {'User-Agent': random_ua()}
 
     def parse_value(self, response, selector, all=False):
         if all:
@@ -18,6 +24,10 @@ class base_spider(object):
             return ''
 
     @property
+    def db(self):
+        return MONGO_CLIENT[DB_NAME]
+
+    @property
     def random_header(self):
         self.HEADERS.update({'User-Agent': random_ua()})
         return self.HEADERS
@@ -25,3 +35,12 @@ class base_spider(object):
     def p_get(self, url):
         headers = self.random_header
         return requests.get(url, headers=headers)
+
+    def generate_key(self):
+        return '{website}_{key_name}'.format(website=self.website, key_name=self.key_name)
+
+    def save_doc(self, coll, doc):
+        try:
+            coll.insert_one(doc)
+        except Exception as e:
+            print(e)
